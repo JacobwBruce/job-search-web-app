e6e888850e82bf7a8dcb345dd7fcd9be531f885d
JacobwBruce <jacobwbruce@hotmail.com>
2021-04-27 17:34:59 -0400

    Homepage cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,13 @@
-import { useAuth } from '@/lib/auth';
 import React from 'react';
-import Link from 'next/link';
-import firebase from 'firebase/app';
-import 'firebase/auth';
+import Header from '@/components/header';
 
 export default function Home() {
-    const { user } = useAuth();
-    console.log(user);
     return (
-        <div>
-            <h1>Home page</h1>
-            <p>{`User ID: ${user ? user.uid : 'no user signed in'} `}</p>
+        <>
+            <Header />
             <div>
-                <Link href='/login'>Go Login</Link>
+                <h1 className='text-4xl mt-10 text-center'>Home Page</h1>
             </div>
-            <div>
-                <Link href='/authenticated'>Authenticated route</Link>
-            </div>
-            <button onClick={async () => firebase.auth().signOut()}>Sign Out</button>
-        </div>
+        </>
     );
 }
