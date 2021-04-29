d38afffe3c61850688d03dc5953d86857c338983
JacobwBruce <jacobwbruce@hotmail.com>
2021-04-29 14:05:42 -0400

    extracted job search form to a new component

---
+++ components/JobResults.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import JobSearchForm from './JobSearchForm';
+
+const JobResults = () => {
+    return (
+        <div className='flex justify-center mt-5 w-full'>
+            <JobSearchForm />
+        </div>
+    );
+};
+
+export default JobResults;
