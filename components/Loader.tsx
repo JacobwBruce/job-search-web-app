b24089d59063145850e166b4984caf2b55c52485
JacobwBruce <jacobwbruce@hotmail.com>
2021-10-22 15:51:22 -0400

    New loading animation

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,7 +1,13 @@
 import React, { FC } from 'react';
 
 const Loader: FC = () => {
-    return <div>Loading...</div>;
+    return (
+        <div className='flex items-center justify-center space-x-2 animate-pulse h-full'>
+            <div className='w-8 h-8 bg-blue-400 rounded-full'></div>
+            <div className='w-8 h-8 bg-blue-400 rounded-full'></div>
+            <div className='w-8 h-8 bg-blue-400 rounded-full'></div>
+        </div>
+    );
 };
 
 export default Loader;
