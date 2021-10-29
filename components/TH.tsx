252137577a3fd417b97e6cd5a65fb9056c90fcd4
JacobwBruce <jacobwbruce@hotmail.com>
2021-10-28 20:35:30 -0400

    Added ability to center text on TH component

--- components/TH.tsx
+++ components/TH.tsx
@@ -1,10 +1,16 @@
 import React, { FC } from 'react';
 
-const TH: FC = ({ children }) => {
+interface Props {
+    center?: boolean;
+}
+
+const TH: FC<Props> = ({ children, center }) => {
     return (
         <th
             scope='col'
-            className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
+            className={`px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider ${
+                center ? 'text-center' : 'text-left'
+            }`}
         >
             {children}
         </th>
