ebb781d34497953911d8a4161203d3d8df0b5416
JacobwBruce <jacobwbruce@hotmail.com>
2021-10-29 08:49:53 -0400

    Added confirm delete Modal to dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { verifyIdToken } from '@/lib/firebaseAdmin';
 import Header from '@/components/Header';
 import Head from 'next/head';
-import { getUserBookmarks } from '@/lib/db';
+import { deleteJobBookmark, getUserBookmarks } from '@/lib/db';
 import Loader from '@/components/Loader';
 import { JobType } from 'types/JobType';
 import Table from '@/components/Table';
@@ -14,9 +14,16 @@ import TableBody from '@/components/TableBody';
 import TD from '@/components/TD';
 import { formatIsoDate } from 'utilities/formatIsoDate';
 import StatusDropDown from '@/components/StatusDropDown';
+import Dropdown from '@/components/Dropdown';
+import { BsThreeDotsVertical } from 'react-icons/bs';
+import DropdownItem from '@/components/DropdownItem';
+import Modal from '@/components/Modal';
+import { IoIosAlert } from 'react-icons/io';
 
 const dashboard = ({ user }) => {
     const [bookmarks, setBookmarks] = useState<Array<JobType>>([]);
+    const [selectedJob, setSelectedJob] = useState<JobType | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
 
     useEffect(() => {
         getBookmarks();
@@ -26,6 +33,14 @@ const dashboard = ({ user }) => {
         const data = await getUserBookmarks(user.uid);
         //@ts-ignore
         setBookmarks(data);
+        setLoading(false);
+    };
+
+    const removeJob = async (job: JobType) => {
+        setLoading(true);
+        await deleteJobBookmark(user.uid, job.id);
+        setSelectedJob(null);
+        getBookmarks();
     };
 
     if (user) {
@@ -35,62 +50,97 @@ const dashboard = ({ user }) => {
                     <title>Dashboard</title>
                 </Head>
                 <Header />
-                <div>
-                    <h1 className='text-4xl mt-10 text-center mb-6'>Saved Job Postings</h1>
-                </div>
-                <div className='w-full flex justify-center'>
-                    <Table>
-                        <TableHead>
-                            <tr>
-                                <TH>Position</TH>
-                                <TH>Company</TH>
-                                <TH>Date Saved</TH>
-                                <TH center={true}>Status</TH>
-                                <th scope='col' className='relative px-6 py-3'>
-                                    <span className='sr-only'>Link</span>
-                                </th>
-                            </tr>
-                        </TableHead>
-                        <TableBody>
-                            {bookmarks.map((bookmark) => (
-                                <tr key={bookmark.id}>
-                                    <TD>
-                                        <div className='text-sm font-medium text-gray-900'>
-                                            {bookmark.title}
-                                        </div>
-                                    </TD>
-                                    <TD>
-                                        <div className='text-sm text-gray-900'>
-                                            {bookmark.company.display_name}
-                                        </div>
-                                        <div className='text-sm text-gray-500'>
-                                            {bookmark.location.display_name}
-                                        </div>
-                                    </TD>
-                                    <TD>
-                                        <div className='text-sm text-gray-900'>
-                                            {formatIsoDate(bookmark.created)}
-                                        </div>
-                                    </TD>
-                                    <TD>
-                                        <StatusDropDown status={bookmark.status || 'No Status'} />
-                                    </TD>
-                                    <TD>
-                                        <div className='text-right text-sm font-medium'>
-                                            <a
-                                                href={bookmark.redirect_url}
-                                                target='__blank__'
-                                                className='text-indigo-600 hover:text-indigo-900'
-                                            >
-                                                Visit
-                                            </a>
-                                        </div>
-                                    </TD>
-                                </tr>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </div>
+
+                {selectedJob && (
+                    <Modal
+                        title='Delete Job?'
+                        icon={<IoIosAlert className='h-6 w-6 text-red-600' aria-hidden='true' />}
+                        color='red'
+                        buttonText='Delete'
+                        confirmCallback={() => removeJob(selectedJob)}
+                        cancelCallback={() => setSelectedJob(null)}
+                    >
+                        Are you sure you want to delete{' '}
+                        <span className='font-medium'>{selectedJob.title}</span> posting from{' '}
+                        <span className='font-medium'>{selectedJob.company.display_name}</span>?
+                    </Modal>
+                )}
+
+                {loading ? (
+                    <Loader />
+                ) : (
+                    <>
+                        <div>
+                            <h1 className='text-4xl mt-10 text-center mb-6'>Saved Job Postings</h1>
+                        </div>
+                        <div className='w-full flex justify-center'>
+                            <Table>
+                                <TableHead>
+                                    <tr>
+                                        <TH>Position</TH>
+                                        <TH>Company</TH>
+                                        <TH>Date Saved</TH>
+                                        <TH center={true}>Status</TH>
+                                        <th scope='col' className='relative px-6 py-3'>
+                                            <span className='sr-only'>Link</span>
+                                        </th>
+                                    </tr>
+                                </TableHead>
+                                <TableBody>
+                                    {bookmarks.map((bookmark) => (
+                                        <tr key={bookmark.id}>
+                                            <TD>
+                                                <div className='text-sm font-medium text-gray-900'>
+                                                    {bookmark.title}
+                                                </div>
+                                            </TD>
+                                            <TD>
+                                                <div className='text-sm text-gray-900'>
+                                                    {bookmark.company.display_name}
+                                                </div>
+                                                <div className='text-sm text-gray-500'>
+                                                    {bookmark.location.display_name}
+                                                </div>
+                                            </TD>
+                                            <TD>
+                                                <div className='text-sm text-gray-900'>
+                                                    {formatIsoDate(bookmark.created)}
+                                                </div>
+                                            </TD>
+                                            <TD>
+                                                <StatusDropDown status={bookmark.status} />
+                                            </TD>
+                                            <TD>
+                                                <div className='flex flex-row align-middle'>
+                                                    <div className='text-right text-sm font-medium'>
+                                                        <a
+                                                            href={bookmark.redirect_url}
+                                                            target='__blank__'
+                                                            className='text-indigo-600 hover:text-indigo-900'
+                                                        >
+                                                            Visit
+                                                        </a>
+                                                    </div>
+                                                    <div className='ml-2'>
+                                                        <Dropdown text={<BsThreeDotsVertical />}>
+                                                            <DropdownItem
+                                                                onClick={() =>
+                                                                    setSelectedJob(bookmark)
+                                                                }
+                                                            >
+                                                                Delete
+                                                            </DropdownItem>
+                                                        </Dropdown>
+                                                    </div>
+                                                </div>
+                                            </TD>
+                                        </tr>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </div>
+                    </>
+                )}
             </>
         );
     } else {
