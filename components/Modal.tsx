64bcc3b7765f0c5b7f1d9271b2cf03aa4d63c617
JacobwBruce <jacobwbruce@hotmail.com>
2021-10-28 20:53:23 -0400

    Minor Modal change

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -6,9 +6,10 @@ interface Props {
     icon: ReactElement;
     color: string;
     callback?: Function;
+    buttonText: string;
 }
 
-const Modal: FC<Props> = ({ title, icon, color, children, callback }) => {
+const Modal: FC<Props> = ({ title, icon, color, children, callback, buttonText }) => {
     const [open, setOpen] = useState(true);
     const [colorClassesOne, setColorClassesOne] = useState<string>('');
     const [colorClassesTwo, setColorClassesTwo] = useState<string>('');
@@ -124,7 +125,7 @@ const Modal: FC<Props> = ({ title, icon, color, children, callback }) => {
                                     className={`w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 ${colorClassesOne} text-base font-medium text-white focus:outline-none focus:ring-2 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm`}
                                     onClick={action}
                                 >
-                                    Deactivate
+                                    {buttonText}
                                 </button>
                                 <button
                                     type='button'
