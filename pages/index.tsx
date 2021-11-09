4ed6de023ddc7f0fea24a06d4f34a6220d717ece
JacobwBruce <jacobwbruce@hotmail.com>
2021-11-09 18:59:14 -0500

    landing page design

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,8 +9,21 @@ export default function Home() {
                 <title>JobSearch</title>
             </Head>
             <Header />
-            <div>
-                <h1 className='text-4xl mt-10 text-center'>Home Page</h1>
+            <div className='h-5/6 flex items-center'>
+                <div className='flex flex-col lg:flex-row justify-items-center mx-28 md:mx-10'>
+                    <h1 className='text-4xl mt-10 text-center font-bold mx-10 lg:text-right'>
+                        Welcome to{' '}
+                        <span className='font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-indigo-600 to-red-500'>
+                            JobSearch!
+                        </span>{' '}
+                        <br />A student project for searching, organizing and keeping track of job
+                        listings
+                    </h1>
+
+                    <div className='text-center'>
+                        <img className='max-w-lg' src='/control_panel.svg' alt='control panel' />
+                    </div>
+                </div>
             </div>
         </>
     );
